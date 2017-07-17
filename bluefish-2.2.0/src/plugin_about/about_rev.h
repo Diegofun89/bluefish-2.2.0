f06886ed2d850ea119d1bc2c8bfc094ff11447d4
Diegofun89 <diegofun89@hotmail.it>
2017-07-17 12:50:14 +0200

    Add existing file

---
+++ bluefish-2.2.0/src/plugin_about/about_rev.h
@@ -0,0 +1,9 @@
+/* Automatically created by the Makefile.
+ * http://bugzilla.gnome.org/show_bug.cgi?id=578719 */
+#ifndef __ABOUT_REV_H_
+#define __ABOUT_REV_H_
+#ifdef SVN_REVISION
+#undef SVN_REVISION
+#endif /* SVN_REVISION */
+#define SVN_REVISION "7003"
+#endif /* __ABOUT_REV_H_ */
